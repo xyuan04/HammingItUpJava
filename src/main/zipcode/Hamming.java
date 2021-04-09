e8cefa77cbf83cb04c2fac803295b6dfa10725b3
xiong <xiong@zipcodes-MBP-8.fios-router.home>
2021-04-09 18:03:52 -0400

    finished lab

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,26 @@
 package zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
+    String hammy1;
+    String hammy2;
+
+    public Hamming(String s, String s1) throws IllegalArgumentException {
+        this.hammy1 = s;
+        this.hammy2 = s1;
+
+        if(hammy1.length() != hammy2.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+        int counter = 0;
+
+            for (int i = 0; i < hammy1.length(); i++) {
+                if (hammy1.charAt(i) != hammy2.charAt(i)) {
+                    counter++;
+                }
+            }
+            return counter;
     }
 }
